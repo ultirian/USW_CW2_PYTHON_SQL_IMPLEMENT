4826cac6411254bf85222171d405d3c77c0dbf81
Chris Weaver <56263796+ultirian@users.noreply.github.com>
2021-07-03 12:51:59 +0100

    Update base.py

--- base.py
+++ base.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Define the MYSQL Engine using MySQL Connector/Python
-engine = create_engine('mysql+mysqlconnector://testuser:Joe!25145309@localhost:3306/user', echo=False)
+engine = create_engine('mysql+mysqlconnector://Bufff', echo=False)
 
 # Create a Session
 Session = sessionmaker(bind=engine)
